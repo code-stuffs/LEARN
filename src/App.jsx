9ad70bd878479d3f5efce7f760d2c63567fea8e6
code-stuffs <164122472+code-stuffs@users.noreply.github.com>
2024-06-15 10:21:24 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Saniya Chivate",
+  title: "Data Analysis",
+  email: "saniyachivate@gmail.com",
+  gitHub: "code-stuffs",
+  instagram: " ",
+  linkedIn: "saniyachivate",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "saniyachivate",
+  youTube: " ",
 };
 
 const primaryColor = "#4E567E";
